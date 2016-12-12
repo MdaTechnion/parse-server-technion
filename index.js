225b9834d129ae8413ab10a653c2a54a10e98d93
MdaTechnion <MdaTechnion@gmail.com>
2016-12-12 21:32:55 +0200

    mdagame - keys

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ var express = require('express');
 var ParseServer = require('parse-server').ParseServer;
 var path = require('path');
 
-var databaseUri = process.env.DATABASE_URI || process.env.MONGODB_URI;
+var databaseUri = 'mongodb://heroku_zt24v79t:vmq9hv326uplljpva0o27674rs@ds127998.mlab.com:27998/heroku_zt24v79t';
 
 if (!databaseUri) {
   console.log('DATABASE_URI not specified, falling back to localhost.');
@@ -14,8 +14,8 @@ if (!databaseUri) {
 var api = new ParseServer({
   databaseURI: databaseUri || 'mongodb://localhost:27017/dev',
   cloud: process.env.CLOUD_CODE_MAIN || __dirname + '/cloud/main.js',
-  appId: process.env.APP_ID || 'myAppId',
-  masterKey: process.env.MASTER_KEY || '', //Add your master key here. Keep it secret!
+  appId: process.env.APP_ID || 'IX6J3Z9jMLoGroGHtuGKBt0xj8Fkt9dV93MjNczO',
+  masterKey: process.env.MASTER_KEY || 'NT5BWgGOTC5LdEBgc1mEhHK4Y13iO5FcK3lGXbQS', //Add your master key here. Keep it secret!
   serverURL: process.env.SERVER_URL || 'http://localhost:1337/parse',  // Don't forget to change to https if needed
   liveQuery: {
     classNames: ["Posts", "Comments"] // List of classes to support for query subscriptions
@@ -31,7 +31,7 @@ var app = express();
 app.use('/public', express.static(path.join(__dirname, '/public')));
 
 // Serve the Parse API on the /parse URL prefix
-var mountPath = process.env.PARSE_MOUNT || '/parse';
+var mountPath = process.env.PARSE_MOUNT || '/';
 app.use(mountPath, api);
 
 // Parse Server plays nicely with the rest of your web routes
